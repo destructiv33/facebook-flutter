7e9803dae80bfd9c954f414fa502907664b3f7e9
destructiv33 <osmaritur@gmail.com>
2021-08-19 17:48:53 -0500

    Flutter project

---
+++ lib/data/data.dart
@@ -0,0 +1,226 @@
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+
+final User currentUser = User(
+  name: 'Destructive MX',
+  imageUrl: 'https://picsum.photos/200/300',
+);
+final List<User> onlineUsers = [
+  User(
+    name: 'Carlos Cordero',
+    imageUrl:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Paola Castro',
+    imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Jara Andreu',
+    imageUrl:
+        'https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1331&q=80',
+  ),
+  User(
+    name: 'Rosalva Jordán',
+    imageUrl:
+        'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+  ),
+  User(
+    name: 'José Luis',
+    imageUrl:
+        'https://images.unsplash.com/photo-1521119989659-a83eee488004?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=664&q=80',
+  ),
+  User(
+    name: 'Eulalia Carretero',
+    imageUrl:
+        'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Lina de Novoa',
+    imageUrl:
+        'https://images.unsplash.com/photo-1519631128182-433895475ffe?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  ),
+  User(
+      name: 'Fabio Losa',
+      imageUrl:
+          'https://images.unsplash.com/photo-1515077678510-ce3bdf418862?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=675&q=80'),
+  User(
+    name: 'Julieta de Gimenez',
+    imageUrl:
+        'https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=592&q=80',
+  ),
+  User(
+    name: 'Marisol Iriarte',
+    imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Andrea Gómez',
+    imageUrl:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Dimas Nieto',
+    imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Riba Chacón',
+    imageUrl:
+        'https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1331&q=80',
+  ),
+  User(
+    name: 'Gabriel Guzman',
+    imageUrl:
+        'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+  ),
+  User(
+    name: 'Martin Alcalde',
+    imageUrl:
+        'https://images.unsplash.com/photo-1521119989659-a83eee488004?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=664&q=80',
+  ),
+  User(
+    name: 'Carolyn Duncan',
+    imageUrl:
+        'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Montserrat Peñas',
+    imageUrl:
+        'https://images.unsplash.com/photo-1519631128182-433895475ffe?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  ),
+  User(
+      name: 'Rafaela Álvaro',
+      imageUrl:
+          'https://images.unsplash.com/photo-1515077678510-ce3bdf418862?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=675&q=80'),
+  User(
+    name: 'Rodolfo Alvarado',
+    imageUrl:
+        'https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=592&q=80',
+  ),
+  User(
+    name: 'Amalia Bautista',
+    imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+];
+
+final List<Story> stories = [
+  Story(
+    user: onlineUsers[2],
+    imageUrl:
+        'https://images.unsplash.com/photo-1498307833015-e7b400441eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80',
+  ),
+  Story(
+    user: onlineUsers[6],
+    imageUrl:
+        'https://images.unsplash.com/photo-1499363536502-87642509e31b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[3],
+    imageUrl:
+        'https://images.unsplash.com/photo-1497262693247-aa258f96c4f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=624&q=80',
+  ),
+  Story(
+    user: onlineUsers[9],
+    imageUrl:
+        'https://images.unsplash.com/photo-1496950866446-3253e1470e8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[7],
+    imageUrl:
+        'https://images.unsplash.com/photo-1475688621402-4257c812d6db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
+  ),
+  Story(
+    user: onlineUsers[2],
+    imageUrl:
+        'https://images.unsplash.com/photo-1498307833015-e7b400441eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80',
+  ),
+  Story(
+    user: onlineUsers[6],
+    imageUrl:
+        'https://images.unsplash.com/photo-1499363536502-87642509e31b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[3],
+    imageUrl:
+        'https://images.unsplash.com/photo-1497262693247-aa258f96c4f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=624&q=80',
+  ),
+  Story(
+    user: onlineUsers[9],
+    imageUrl:
+        'https://images.unsplash.com/photo-1496950866446-3253e1470e8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[7],
+    imageUrl:
+        'https://images.unsplash.com/photo-1475688621402-4257c812d6db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
+  ),
+];
+
+final List<Post> posts = [
+  Post(
+    user: currentUser,
+    caption: 'Noche de michis :v',
+    timeAgo: '58m',
+    imageUrl:
+        'https://t2.ea.ltmcdn.com/es/images/5/7/5/como_socializar_un_gato_cachorro_22575_600_square.jpg',
+    likes: 1202,
+    comments: 184,
+    shares: 96,
+  ),
+  Post(
+    user: onlineUsers[5],
+    caption:
+        'Aquella noche fue épica; llena de incertidumbre, beneficios, dudas, preguntas y respuestas. Fue una noche llena de frutos, llena de pensamientos y de ideas. Aquella noche quedará como un grato recuerdo de lo que fui y lo que comenzaré a ser de aquí en adelante. Es el momento en el que todo hombre llega y debe decidir entre quedarse donde está o comenzar una nueva vida llena de retos e incertidumbre plena pero satisfactoria.',
+    timeAgo: '3hr',
+    imageUrl: null,
+    likes: 683,
+    comments: 79,
+    shares: 18,
+  ),
+  Post(
+    user: onlineUsers[4],
+    caption: 'El chems IRL.',
+    timeAgo: '8hr',
+    imageUrl:
+        'https://images.unsplash.com/photo-1575535468632-345892291673?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    likes: 894,
+    comments: 201,
+    shares: 27,
+  ),
+  Post(
+    user: onlineUsers[3],
+    caption: 'Aventura 🏔',
+    timeAgo: '15hr',
+    imageUrl:
+        'https://images.unsplash.com/photo-1573331519317-30b24326bb9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    likes: 722,
+    comments: 183,
+    shares: 42,
+  ),
+  Post(
+    user: onlineUsers[0],
+    caption:
+        'La vida se vuelve más fácil cuando aprendes a aceptar las disculpas que nunca recibiste',
+    timeAgo: '1d',
+    imageUrl: null,
+    likes: 482,
+    comments: 37,
+    shares: 9,
+  ),
+  Post(
+    user: onlineUsers[9],
+    caption: 'Un clasico',
+    timeAgo: '1d',
+    imageUrl:
+        'https://images.unsplash.com/reserve/OlxPGKgRUaX0E1hg3b3X_Dumbo.JPG?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    likes: 1523,
+    shares: 129,
+    comments: 301,
+  )
+];
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+import 'package:flutter_facebook_responsive_ui/data/data.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TrackingScrollController _trackingScrollController =
+      TrackingScrollController();
+  @override
+  void dispose() {
+    _trackingScrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onDoubleTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Responsive(
+          mobile: _HomeScreenMobile(
+            scrollController: _trackingScrollController,
+          ),
+          desktop: _HomeScreenDesktop(
+            scrollController: _trackingScrollController,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _HomeScreenDesktop extends StatelessWidget {
+  final TrackingScrollController scrollController;
+
+  const _HomeScreenDesktop({
+    Key key,
+    @required this.scrollController,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
+
+class _HomeScreenMobile extends StatelessWidget {
+  final TrackingScrollController scrollController;
+
+  const _HomeScreenMobile({
+    Key key,
+    @required this.scrollController,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      controller: scrollController,
+      slivers: [
+        SliverAppBar(
+          brightness: Brightness.light,
+          backgroundColor: Colors.white,
+          title: Text(
+            'facebook',
+            style: const TextStyle(
+              color: Palette.facebookBlue,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: -1.2,
+            ),
+          ),
+          centerTitle: false,
+          floating: true,
+          actions: [
+            CircleButton(icon: Icons.search, iconSize: 30, onPressed: () => {}),
+            CircleButton(
+                icon: MdiIcons.facebookMessenger,
+                iconSize: 30,
+                onPressed: () => {}),
+          ],
+        ),
+        SliverToBoxAdapter(
+          child: CreatePostContainer(currentUser: currentUser),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+          sliver: SliverToBoxAdapter(
+            child: Rooms(onlineUsers: onlineUsers),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
+          sliver: SliverToBoxAdapter(
+            child: Stories(
+              currentUser: currentUser,
+              stories: stories,
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              final Post post = posts[index];
+              return PostContainer(post: post);
+            },
+            childCount: posts.length,
+          ),
+        )
+      ],
+    );
+  }
+}
